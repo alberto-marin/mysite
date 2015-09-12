7eee155403dce3852be270e9b81cfe99e81d1d41
alberto <hello@albertomar.in>
2015-09-12 19:17:18 +0200

    yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name probandoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the probandoApp
+ */
+angular.module('probandoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name probandoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the probandoApp
+ */
+angular.module('probandoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
